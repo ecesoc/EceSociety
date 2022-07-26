870abb175e8e8d1fa8744e0485500ebc3b18e2a1
Kshitij Sharma <kshitijsharma1908@gmail.com>
2022-07-26 12:55:43 +0530

    executive-body-k20

--- src/Components/Techathon/SpeakerCard.jsx
+++ src/Components/Techathon/SpeakerCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 export default function SpeakerCard({ member }) {
   const { image, title, description, link, name } = member;
@@ -33,5 +33,31 @@ export default function SpeakerCard({ member }) {
         {description}
       </p>
     </div>
+  );
+}*/
+import React from "react";
+export default function SpeakerCard({ member }) {
+  const { image, title, description, name,link } = member;
+
+  return (
+    <a className='speaker-link' href={link}>
+    <div className="speaker-card" >
+      <div className="speaker-pic-info">
+      <img
+        className="speaker-pic"
+        alt="not found"
+        src={`/images/members/${image}`}
+      />
+       <p className="speaker-description">"{description}"</p>
+      </div>
+
+      <div className="speaker-details">
+      <h2 className="speaker-name">{name}</h2>
+      <div className="speaker-info">
+        <h5 className="speaker-title">{title}</h5>
+      </div>
+      </div>
+    </div>
+    </a>
   );
 }
